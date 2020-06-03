9e6043af77373a5674d21832127c034869b083ec
David Pfahler <pfahler.david@gmail.com>
2020-06-03 11:37:36 +0200

    added the date when the post was made

--- src/post.jsx
+++ src/post.jsx
@@ -4,7 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
+import Box from '@material-ui/core/Box';
+import TimeAgo from 'react-timeago'
 
 import Vote from './vote';
 
@@ -14,22 +15,32 @@ const styles = theme => ({
     flexGrow: 1,
   },
   box: {
-    margin: theme.spacing(2),
+    margin: theme.spacing(1),
     padding: theme.spacing(2),
-    display: "flex",
-  },
+  }
 });
+
+const Ago = (props) => {
+  return <Grid xs={12}>
+    <Typography variant="caption" color="inherit">
+      <TimeAgo date={props.time} />
+    </Typography>
+  </Grid>
+}
 
 const Post = (props) => {
   const { id, post, user, classes } = props;
-  const { text } = post;
+  const { text, time } = post;
 
   return <Grid item xs={12} sm={6} lg={4} className={classes.root}>
     <Paper square elevation={2} className={classes.box}>
-      <Typography variant="h6" color="inherit">
-        {text}
-      </Typography>
-      <Vote post={post} id={id} user={user} />
+      <Box display="flex" >
+        <Typography variant="h6" color="inherit">
+          {text}
+        </Typography>
+        <Vote post={post} id={id} user={user} />
+      </Box>
+      <Ago time={time} />
     </Paper>
   </Grid>
 };
